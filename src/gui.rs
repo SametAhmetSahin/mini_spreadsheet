3725b9b47efb14c68a748a61a16b631ee9c6977a
Doruk <doruk.cem@gmail.com>
2025-01-21 18:13:08 +0300

    labels

--- src/gui.rs
+++ src/gui.rs
@@ -28,10 +28,19 @@ const SELECTED_CELL_BORDER_COLOR: Color = ORANGE;
 const NORMAL_CELL_BORDER_COLOR: Color = BLACK;
 const CELL_TEXT_COLOR: Color = BLACK;
 
+// Labels
+const ROW_LABEL_WIDTH: f32 = 40.0;
+const COL_LABEL_HEIGHT: f32 = 30.0;
+const LABEL_FONT_SIZE: u16 = 10;
+const LABEL_TEXT_COLOR: Color = DARKGRAY;
+const LABEL_BORDER_COLOR: Color = DARKGRAY;
+const SELECTED_LABEL_BACKGROUND: Color = SKYBLUE;
+
 pub struct GUI {
     selected_cell: Option<Index>,
     editor_content: String,
-    font: Font,
+    regular_font: Font,
+    bold_font: Font,
     spread_sheet: SpreadSheet,
 }
 
@@ -40,11 +49,17 @@ impl GUI {
         let font = load_ttf_font("fonts/jetbrains-mono-font/JetbrainsMonoRegular-RpvmM.ttf")
             .await
             .unwrap();
+
+        let bold = load_ttf_font("fonts/jetbrains-mono-font/JetbrainsMonoBold-51Xez.ttf")
+            .await
+            .unwrap();
+
         Self {
             selected_cell: None,
-            font,
+            regular_font: font,
             editor_content: String::new(),
             spread_sheet,
+            bold_font: bold,
         }
     }
 
@@ -94,19 +109,33 @@ impl GUI {
         let (start_x, start_y) = start;
         let (end_x, end_y) = end;
 
-        let cell_height = (end_y - start_y) / GRID_ROWS as f32;
-        let cell_width = (end_x - start_x) / GRID_COLS as f32;
+        let grid_height = end_y - start_y - COL_LABEL_HEIGHT;
+        let grid_width = end_x - start_x - ROW_LABEL_WIDTH;
+
+        let cell_height = grid_height / GRID_ROWS as f32;
+        let cell_width = grid_width / GRID_COLS as f32;
 
         // Handle if mouse clicked
         if is_mouse_button_pressed(MouseButton::Left) {
             let (x, y) = mouse_position();
             if is_point_in_rect((x, y), start, end) {
-                let col = ((x - start_x) / cell_width) as i32;
-                let row = ((y - start_y) / cell_height) as i32;
-                self.change_selected_cell(Index {
-                    x: col.try_into().expect("Got negative idx from click"),
-                    y: row.try_into().expect("Got negative idx from click"),
-                });
+                let col = ((x - start_x - ROW_LABEL_WIDTH) / cell_width) as i32;
+                let row = ((y - start_y - COL_LABEL_HEIGHT) / cell_height) as i32;
+                let x_idx = col.try_into().expect("Got negative idx from click");
+                let y_idx = row.try_into().expect("Got negative idx from click");
+                if is_key_down(KeyCode::LeftControl) {
+                    if let Some(_) = self.selected_cell {
+                        if &Some('=') == &self.editor_content.chars().nth(0) {
+                            self.editor_content.push_str(&format!(
+                                "{}{}",
+                                column_idx_to_string(x_idx),
+                                y_idx + 1
+                            ))
+                        }
+                    }
+                } else {
+                    self.change_selected_cell(Index { x: x_idx, y: y_idx });
+                }
             }
         }
 
@@ -119,16 +148,47 @@ impl GUI {
             GRID_BACKGROUND_COLOR,
         );
 
+        // Draw the column labels
+        for col in 0..GRID_COLS {
+            let label_start_x = start_x + col as f32 * cell_width + ROW_LABEL_WIDTH;
+            let label_start_y = start_y;
+            self.draw_label(
+                col,
+                false, // Indicating column
+                (label_start_x, label_start_y),
+                (cell_width, COL_LABEL_HEIGHT),
+            );
+        }
+
+        // Draw the row labels
+        for row in 0..GRID_ROWS {
+            let label_start_x = start_x;
+            let label_start_y = start_y + row as f32 * cell_height + COL_LABEL_HEIGHT;
+            self.draw_label(
+                row,
+                true, // Indicating row
+                (label_start_x, label_start_y),
+                (ROW_LABEL_WIDTH, cell_height),
+            );
+        }
+
         // Draw all cells in the grid
         for row in 0..GRID_ROWS {
             for col in 0..GRID_COLS {
-                let cell_start_x = start_x + col as f32 * cell_width;
-                let cell_start_y = start_y + row as f32 * cell_height;
+                let cell_start_x = start_x + col as f32 * cell_width + ROW_LABEL_WIDTH;
+                let cell_start_y = start_y + row as f32 * cell_height + COL_LABEL_HEIGHT;
+
+                // Adjust the height of the last row to account for any floating-point error
+                let adjusted_cell_height = if row == GRID_ROWS - 1 {
+                    grid_height - (row as f32 * cell_height)
+                } else {
+                    cell_height
+                };
 
                 self.draw_cell(
                     Index { x: col, y: row },
                     (cell_start_x, cell_start_y),
-                    (cell_width, cell_height),
+                    (cell_width, adjusted_cell_height),
                 );
             }
         }
@@ -156,7 +216,7 @@ impl GUI {
         };
 
         if !text.is_empty() {
-            let text_dimensions = measure_text(text, Some(&self.font), CELL_FONT_SIZE, 1.0);
+            let text_dimensions = measure_text(text, Some(&self.regular_font), CELL_FONT_SIZE, 1.0);
 
             let text_x = center_x - text_dimensions.width / 2.0;
             let text_y = center_y + text_dimensions.height / 2.0; // Adjust y for baseline alignment
@@ -166,7 +226,7 @@ impl GUI {
                 text_x,
                 text_y,
                 TextParams {
-                    font: Some(&self.font),
+                    font: Some(&self.regular_font),
                     font_size: CELL_FONT_SIZE,
                     font_scale: 1.0,
                     font_scale_aspect: 1.0,
@@ -175,6 +235,59 @@ impl GUI {
                 },
             );
         }
+    }
+
+    fn draw_label(&self, idx: usize, is_row: bool, start: (f32, f32), dimensions: (f32, f32)) {
+        let (start_x, start_y) = start;
+        let (width, height) = dimensions;
+        let center_x = start_x + width / 2.0;
+        let center_y = start_y + height / 2.0;
+
+        let is_selected_label = {
+            if let Some(selected) = self.selected_cell {
+                if is_row {
+                    selected.y == idx
+                } else {
+                    selected.x == idx
+                }
+            } else {
+                false
+            }
+        };
+
+        if is_selected_label {
+            // Draw background
+            draw_rectangle(start_x, start_y, width, height, SELECTED_LABEL_BACKGROUND);
+        }
+
+        draw_rectangle_lines(start_x, start_y, width, height, 1.0, LABEL_BORDER_COLOR);
+        let text = if is_row {
+            idx.to_string()
+        } else {
+            column_idx_to_string(idx)
+        };
+        let text_dimensions = measure_text(&text, Some(&self.regular_font), LABEL_FONT_SIZE, 1.0);
+
+        let text_x = center_x - text_dimensions.width / 2.0;
+        let text_y = center_y + text_dimensions.height / 2.0; // Adjust y for baseline alignment
+
+        draw_text_ex(
+            &text,
+            text_x,
+            text_y,
+            TextParams {
+                font: Some(if is_selected_label {
+                    &self.bold_font
+                } else {
+                    &self.regular_font
+                }),
+                font_size: LABEL_FONT_SIZE,
+                font_scale: 1.0,
+                font_scale_aspect: 1.0,
+                rotation: 0.0,
+                color: LABEL_TEXT_COLOR,
+            },
+        );
     }
 
     fn commit_editor(&mut self) {
@@ -207,6 +320,21 @@ impl GUI {
     }
 }
 
+fn column_idx_to_string(mut idx: usize) -> String {
+    let mut s = String::new();
+
+    loop {
+        let rem = (idx % 26) as u8;
+        s.insert(0, (b'A' + rem) as char); // Prepend the character
+        if idx < 26 {
+            break;
+        }
+        idx = idx / 26 - 1;
+    }
+
+    s
+}
+
 fn is_point_in_rect<T: std::cmp::PartialOrd>(
     point: (T, T),
     rect_start: (T, T),
@@ -218,12 +346,12 @@ fn is_point_in_rect<T: std::cmp::PartialOrd>(
         && point.1 <= rect_end.1
 }
 
-/*  
-    Format a float into scientific notation such as: 42.0 -> 4.200e+01
-    width controls the amount of left padded spaces
-    precision is the amount of decimals
-    exp_pad controls the amount of left padded 0s
- */
+/*
+   Format a float into scientific notation such as: 42.0 -> 4.200e+01
+   width controls the amount of left padded spaces
+   precision is the amount of decimals
+   exp_pad controls the amount of left padded 0s
+*/
 fn fmt_f64(num: f64, width: usize, precision: usize, exp_pad: usize) -> String {
     let mut num = format!("{:.precision$e}", num, precision = precision);
     // Safe to `unwrap` as `num` is guaranteed to contain `'e'`
@@ -244,11 +372,13 @@ fn computed_to_text(computed: Option<Result<Value, ComputeError>>) -> String {
         Some(value) => match value {
             Ok(inner) => match inner {
                 Value::Text(s) => s,
-                Value::Number(num) => if num >= 1E15 {
-                    fmt_f64(num, 10, 3, 2)
-                } else {
-                    num.to_string()
-                },
+                Value::Number(num) => {
+                    if num >= 1E15 {
+                        fmt_f64(num, 10, 3, 2)
+                    } else {
+                        num.to_string()
+                    }
+                }
                 Value::Bool(b) => b.to_string(),
             },
             Err(err) => err.to_string(),
